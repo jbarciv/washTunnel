1418527ac28911b6ce06c95a678960232ce614b5
gqt124 <gonzaloytorres@gmail.com>
2022-04-23 16:12:32 +0200

    act

--- P1/tunelGotBusy.c
+++ P1/tunelGotBusy.c
@@ -13,25 +13,27 @@ De la apertura de la barrera es encargará otra función que maneje el motor con
 
 // Aunque en esta función solo vamos a utilizar una PCINT, en el setup hay que activarlas todas para poder usarlas en otras funciones
 
+cli();
 
-//DDRJ = 0x80               // (0b10000000) Ponemos los pines J(0:6) como entradas
-//PCICR |= (1<<PCIE1)       // Activamos las PCINT(15:8)
-//PCMSK1 |= (1<<PCINT8)     // Habilitamos la interrupcion 
+DDRJ = 0x80;               // (0b10000000) Ponemos los pines J(0:6) como entradas
+PCICR |= (1<<PCIE1);       // Activamos las PCINT(15:8)
+PCMSK1 |= (1<<PCINT8);     // Habilitamos la interrupcion 
 
-//sei()                     //Habilitamos las interrupciones globales
+sei();                     //Habilitamos las interrupciones globales
 
 //La apertura de barrera debe ser controlada por consulta periódica porque puede que justo cuando llega el coche no pueda ser abierta porque hay otro que acaba de entrar. Al hacerlo por consulta periódica coseguimos que pueda haber un periodo de espera fuera de las interrupciones, simplificándolas.
 
 ISR(PCINT1_vect)
 {
-    if ((PINE&(1<<PINE0) == 1) && ar_E0 > 20)   //Acaba de pasar la parte trasera de un coche dentro del tunel
-    {
-        tunelGotBusy();
-    }
-    
-    
 
-    
+    flag_S01 = 1;
+    if (antireb_S01 > 300 && tPreviousCar >) 
+
+    // if ((PINE&(1<<PINE0) == 1) && ar_E0 > 20)   //Acaba de pasar la parte trasera de un coche dentro del tunel
+    // {
+    //     tunelGotBusy();
+    // }
+        
 }
 
 
