60eeda2e01e79f1f85a66482e7e16da8c1bed527
jbarciv <chemabc@gmail.com>
2022-04-26 16:00:31 +0200

    comentarios.txt some pins added

--- definitivo/commonstuff.h
+++ definitivo/commonstuff.h
@@ -26,56 +26,53 @@
 typedef enum {M1, M2, M3, M4, M5, M6} motor_t;
 typedef enum {ENCENDIDO, APAGADO} motor_status_t;
 typedef enum {DERECHA, IZQUIERDA} direccion_t;
-// Motor  M1 -> DDR# y PORT#
-#define M1ENPORT PORT           //no faltan los DDR?
-#define M1DIPORT PORT
-// Motor  M2 -> DDR# y PORT#
-#define M2ENPORT PORT
-#define M2DIPORT PORT
-// Motor  M3 -> DDR# y PORT# 
-#define M3ENPORT PORT
-#define M3DIPORT PORT
-// Motor  M4 -> DDR# y PORT#
-#define M4ENPORT PORT
-#define M4DIPORT PORT
-// Motor  M5 -> DDR# y PORT#
-#define M5ENPORT PORT
-#define M5DIPORT PORT
-// Motor  M6 -> DDR# y PORT#
-#define M6ENPORT PORT
-#define M6DIPORT PORT
+// Motor  M1 -> BARRERA ENTRADA
+#define M1ENPORT PORTL     
+#define M1DIPORT PORTL
+// Motor  M2 -> LAVADO VERTICAL
+#define M2ENPORT PORTL
+#define M2DIPORT PORTL
+// Motor  M3 -> LAVADO HORIZONTAL
+#define M3ENPORT PORTL
+#define M3DIPORT PORTL
+// Motor  M4 -> GIRO LAVADO HORIZONTAL
+#define M4ENPORT PORTL
+#define M4DIPORT PORTL
+// Motor  M5 -> SECADO
+#define M5ENPORT PORTD
+#define M5DIPORT PORTD
+// Motor  M6 -> CINTA ARRASTRE
+#define M6ENPORT PORTD
+#define M6DIPORT PORTD
 
 /* Etiquetas para los sensores opticos */
-#define SO1  (PIND & 0b00000001) // PD0 -> Sensor de entrada "abajo"
-#define SO2  (PIN & 0b00000100) // P -> Sensor de entrada barrera
-#define SO3  (PIN & 0b00010000) // P -> Sensor lavado horiz. izqda.
-#define SO4  (PIN & 0b01000000) // P -> Sensor lavado horiz. centro
-#define SO5  (PIN & 0b00000001) // P -> Sensor lavado horiz. drcha.
-#define SO6  (PIN & 0b00000100) // P -> ¡!sin utilidad definida
-#define SO7  (PIN & 0b00010000) // P -> Sensor secado izqda.
-#define SO8  (PIN & 0b01000000) // P -> Sensor secado centro.
-#define SO9  (PIN & 0b00000010) // P -> Sensor secado drcha.
-#define SO10 (PIN & 0b00001000) // P -> Sensor de "aviso" de salida
-#define SO11 (PIN & 0b00100000) // P -> ¡!sin utilidad definida
-#define SO12 (PIN & 0b10000000) // P -> Sensor de salida "definitiva"
+#define SO1  (PINK & 0b00000001) // PK0 -> Sensor de entrada "abajo"
+#define SO2  (PINK & 0b00000100) // PK1 -> Sensor de entrada barrera
+#define SO3  (PINK & 0b00010000) // PK2 -> Sensor lavado horiz. izqda.
+#define SO4  (PINK & 0b01000000) // PK3 -> Sensor lavado horiz. centro
+#define SO5  (PINK & 0b00000001) // PK4 -> Sensor lavado horiz. drcha.
+#define SO6  (PINK & 0b00000100) // PK5 -> ¡!sin utilidad definida
+#define SO7  (PINB & 0b00010000) // PB0 -> Sensor secado izqda.
+#define SO8  (PINB & 0b01000000) // PB1 -> Sensor secado centro.
+#define SO9  (PINB & 0b00000010) // PB2 -> Sensor secado drcha.
+#define SO10 (PINB & 0b00001000) // PB3 -> Sensor de "aviso" de salida
+#define SO11 (PINB & 0b00100000) // PB4 -> ¡!sin utilidad definida
+#define SO12 (PINB & 0b10000000) // PB5 -> Sensor de salida "definitiva"
 
 /* Etiquetas para los sensores mecanicos (microinterruptores) */
-#define SW1  (PIN & 0b00000001) // P -> Sensor apertura-cierre de barrera
-#define SW2  (PIN & 0b00000100) // P -> Sensor finales de carrera lavado horiz.
-#define SW3  (PIN & 0b00010000) // P -> Sensor finales de carrera secado
-#define SW4  (PIN & 0b01000000) // P -> Sensor/botón para de emergencia
+#define SW1  (PIND & 0b00000001) // PD0 -> Sensor apertura-cierre de barrera
+#define SW2  (PIND & 0b00000100) // PD1 -> Sensor finales de carrera lavado horiz.
+#define SW3  (PIND & 0b00010000) // PD2 -> Sensor finales de carrera secado
+#define SW4  (PIND & 0b01000000) // PD3 -> Sensor/botón para de emergencia
 #define boton_emergencia SW4
 
 /* Etiquetas para los LEDs */
 #define L1  (PIN & 0b00000010) // P -> LED L1: WAITING or BUSY
 #define L4  (PIN & 0b00001000) // P -> LED L4: semaforo verde
 #define L5  (PIN & 0b00100000) // P -> LED L5: semaforo rojo
-
-// LED L1 -> DDR# y PORT#
+// Puertos para los LEDs
 #define L1PORT PORT
-// LED L4
 #define L4PORT PORT
-// LED L5
 #define L5PORT PORT
 
 
