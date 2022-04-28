8ff6d9638f90b78dd17580e52abdab87f334a330
jbarciv <chemabc@gmail.com>
2022-04-28 07:59:00 +0200

    lavado horizontal motor - ON/OFF

--- P1/lavado_horizontal.c
+++ P1/lavado_horizontal.c
@@ -10,21 +10,21 @@ ISR (PCINT2_vect)       // PCINT(23:16), puerto K
     if(LH==1){
         if(((PINK0&0x20)==0x20)&&((PINK&0x20)==0x00)and((PINK&0x1C)==0x1C)){
             LH=0;
-            Motor(M4,APAGADO,DERECHA);
+            Motor(M4,OFF,DERECHA);
         }else if((PINK&0X14)==0X14){
             if((PINK&0X08)==0X08){
-                Motor(M3,ENCENDIDO,IZQUIERDA);
+                Motor(M3,ON,IZQUIERDA);
             }else{
-                Motor(M3,APAGADO,IZQUIERDA);
+                Motor(M3,OFF,IZQUIERDA);
             }
         }else{
-            Motor(M3,ENCENDIDO,DERECHA);
+            Motor(M3,ON,DERECHA);
         }
     }else if((PINK&0x0C)==0x0C){
-        Motor(M3,ENCENDIDO,DERECHA);
+        Motor(M3,ON,DERECHA);
         else{
             LH=1;
-            Motor(M4,ENCENDIDO,DERECHA);//EL SENTIDO HAY Q MIRARLO
+            Motor(M4,ON,DERECHA);//EL SENTIDO HAY Q MIRARLO
         }
     }
     PINK0=PINK;
