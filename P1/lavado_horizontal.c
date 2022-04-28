96539ae1174e9b632b4a34d9d8c6ff52a40ce717
mariogomezpozo <94970439+mariogomezpozo@users.noreply.github.com>
2022-04-28 12:07:29 +0200

    Cambio en Lavada y secado

--- P1/lavado_horizontal.c
+++ P1/lavado_horizontal.c
@@ -1,30 +1,37 @@
 #include "commonstuff.h"
 #include "lavado_horizontal.h"
-//variable global LH?
+//variable global LH
 //Motor(motor_t motor, status_t estado, direccion_t giro)
-//fucion que realiza el control del lavado por consulta periodica, tiene que estar en el bucle del main
-
-//variariable LH
-ISR (PCINT2_vect)       // PCINT(23:16), puerto K
+ISR (PCINT2_vect)       // PCINT puerto k
 {
-    if(LH==1){
-        if(((PINK0&0x20)==0x20)&&((PINK&0x20)==0x00)and((PINK&0x1C)==0x1C)){
+    if(LH==1){  //Bandera que indica si el lavado horizontal esta activo
+        if(((PINK0&0x20)==0x20)&&((PINK&0x20)==0x00)and((PINK&0x1C)==0x1C)){ 
+            //Se comprueba si ha habido flaNco de bajada en S06 y si S03,S04 y S05 no detectan nada, el coche esta saliendo
             LH=0;
             Motor(M4,OFF,DERECHA);
-        }else if((PINK&0X14)==0X14){
-            if((PINK&0X08)==0X08){
+        }else{
+            if((PINK&0X14)==0X14){  //Se comprueba si S03 y S05 estan detectando algo
+                if((PINK&0X08)==0X08){  //Se comprueba si S04 esta detectando algo
+                    Motor(M3,ON,IZQUIERDA);
+                }else{
+                    Motor(M3,OFF,IZQUIERDA);
+                }
+            }else{
+                Motor(M3,ON,DERECHA);
+            }
+        }
+    }else{
+        if((PINK&0x0C)==0x0C){  //Se comprueba si S03 Y S04 estan detectando algo--> el caso raro
+            if(((PINK0&0x20)==0x20)&&((PINK&0x20)==0x00)and((PINK&0x1C)==0x1C)){ //Se comprueba si el coche que ha ido para atras ha salido
                 Motor(M3,ON,IZQUIERDA);
             }else{
-                Motor(M3,OFF,IZQUIERDA);
+                Motor(M3,ON,DERECHA);   //El coche que ha ido para atras sigue bloqueando
             }
-        }else{
-            Motor(M3,ON,DERECHA);
-        }
-    }else if((PINK&0x0C)==0x0C){
-        Motor(M3,ON,DERECHA);
+            
         else{
             LH=1;
             Motor(M4,ON,DERECHA);//EL SENTIDO HAY Q MIRARLO
+        }
         }
     }
     PINK0=PINK;
--- P1/secado_horizontal.c
+++ P1/secado_horizontal.c
@@ -2,7 +2,37 @@
 #include "secado_horizontal.h"
 //variable global SH?
 //Motor(motor_t motor, status_t estado, direccion_t giro)
-ISR (PCINT3_vect)       // PCINT(23:16), puerto K
+// es posible que este motor tenga que tener PWM
+
+ISR (PCINT3_vect)       // PCINT puerto B
 {
-    
+    if(SH==1){  //Bandera que indica si el secado esta ativo
+        if((PINK&0x80)==0x80){  //Sensor de fin de carrera activo(SW3)
+            SH=0;
+            Motor(M5,OFF,DERECHA);
+        }else{
+            if((PINB&0x05)==0x05){ //Sensores S07 y S09 sin detectar nada
+                if((PINB&0x02)==0x02){ //Sensores S08 sin detectar nada
+                    Motor(M5,ON,IZQUIERDA);
+                }else{
+                    Motor(M5,OFF,IZQUIERDA);
+                }
+            }else{
+                Motor(M5,ON,DERECHA);
+            }
+        }
+    }else{
+        if((PINB&0x03)){    //Se comprueba si S07 y S08 estan activos--> caso raro
+            if((PINK&0x80)==0x80){ //Se comprueba si el secado ha subido hasta arriba
+                Motor(M5,ON,IZQUIERDA);
+            }else{
+                Motor(M5,ON,DERECHA);   //El coche que ha ido para atras sigue bloqueando
+            }
+            
+        else{
+            SH=1;
+        }
+        }
+    }
+    PINB0=PINB;
 }
