43f7802fc15b8bd7f8aeeefc8d402545b58c8809
jbarciv <chemabc@gmail.com>
2022-05-02 13:42:33 +0200

    main before check

--- definitivo/main.c
+++ definitivo/main.c
@@ -1,9 +1,10 @@
 /* ----------------------------------------------------------------------------
- * aquí se define el bucle principal, quizás también el setup
- * 
- * 
- * 
- * 
+ * WASHTUNNEL
+ *  - MAIN
+ *  - SETUP
+ *      - motorSetup()
+ *      - timerSetup()
+ *      - entradaSetup()
  * -------------------------------------------------------------------------- */
 
 #include "commonstuff.h"
@@ -11,7 +12,8 @@
 /********************* Variables globales ***********************************/
 miliseconds_t   miliseconds = 0;
 seconds_t       seconds = 0;
-bool            SO1_f;          // estado real(filtrado) del sensor SO1
+bool            SO1_f;          // Sensor SO1: estado real (filtrado)
+
 
 void motorSetup()
 {
@@ -19,11 +21,35 @@ void motorSetup()
     DDRD |= 0xF0;
 }
 
+void entradaSetup()
+{
+    cli();                      // El SO1 es el DDRD1 y debe ir como entrada(0).
+    PCICR |= (1<<PCIE1);        // Activamos las PCINT(15:8)
+    CMSK1 |= (1<<PCINT8);       // Habilitamos la interrupcion
+    sei();                      //Habilitamos las interrupciones globales
+}
+
+void timerSetup()
+{
+    // setup del contador 3
+    cli ();
+    TCCR3B |= (1<<WGM32);           // Modo ctc
+    TCCR3B |= (1<<CS31)|(1<<CS30);  // Preescalado clk/64 (periodo de 8 uS)
+    OCR3A = ANTIREB_TIME;           // Contamos COUNTER_TIME periodos
+    TIMSK3 |= (1<<OCIE3A);          // Habilitamos la interrupción por compare match
+    // setup del contador 4
+    TCCR3B |= (1<<WGM42);           // Modo ctc
+    TCCR3B |= (1<<CS42);            // Preescalado clk/256 (periodo de 32 uS)
+    OCR3A = REAL_TIME;              // Contamos REAL_TIME periodos (=1 S)
+    TIMSK3 |= (1<<OCIE4A);          // Habilitamos la interrupción por compare match
+    sei();
+}
 
 int main(void)
-{
+{   
+    timerSetup();
     motorSetup();
-    
+    entradaSetup();
     
     while(1)
     {
