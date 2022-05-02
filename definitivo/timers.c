cec7ec1d2931dcf87e1b1b22da1ae4eb0c0b6113
jbarciv <chemabc@gmail.com>
2022-05-02 13:03:22 +0200

    timers.c created

---
+++ definitivo/timers.c
@@ -0,0 +1,23 @@
+/*
+TIMERS PARA LA GESTIÓN DEL TIEMPO
+- "miliseconds" varible global de tipo "unsigned long int", desborda a los 50 días aproximadamente.
+- "seconds" variable global de tipo "unsigned int", desborda a las 18 horas aproximadamente.
+*/
+
+#include "commonstuff.h"
+
+/********************************************
+INTERRUPCIÓN PERIÓDICA CADA 1 MILISEGUNDO
+********************************************/
+ISR (TIMER3_COMPA_vect)
+{
+    miliseconds++;
+}
+
+/********************************************
+INTERRUPCIÓN PERIÓDICA CADA 1 SEGUNDO
+********************************************/
+ISR (TIMER4_COMPA_vect)
+{
+    seconds++;
+}
