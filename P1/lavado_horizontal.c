5a189c0ea0eb41d526ff248b0528374853ed89e4
mariogomezpozo <94970439+mariogomezpozo@users.noreply.github.com>
2022-05-04 13:57:46 +0200

    Motor con minuscula

--- P1/lavado_horizontal.c
+++ P1/lavado_horizontal.c
@@ -2,35 +2,39 @@
 #include "lavado_horizontal.h"
 //variable global LH
 //Motor(motor_t motor, status_t estado, direccion_t giro)
+extern int LH;
+extern int PINK0;
+
 ISR (PCINT2_vect)       // PCINT puerto k
 {
+    
     if(LH==1){  //Bandera que indica si el lavado horizontal esta activo
         if(((PINK0&0x20)==0x20)&&((PINK&0x20)==0x00)&&((PINK&0x1C)==0x1C)){ 
             //Se comprueba si ha habido flaNco de bajada en S06 y si S03,S04 y S05 no detectan nada, el coche esta saliendo
             LH=0;
-            Motor(M4,OFF,DERECHA);
+            motor(M4,OFF,DERECHA);
         }else{
             if((PINK&0X14)==0X14){  //Se comprueba si S03 y S05 estan detectando algo
                 if((PINK&0X08)==0X08){  //Se comprueba si S04 esta detectando algo
-                    Motor(M3,ON,IZQUIERDA);
+                    motor(M3,ON,IZQUIERDA);
                 }else{
-                    Motor(M3,OFF,IZQUIERDA);
+                    motor(M3,OFF,IZQUIERDA);
                 }
             }else{
-                Motor(M3,ON,DERECHA);
+                motor(M3,ON,DERECHA);
             }
         }
     }else{
         if((PINK&0x0C)==0x0C){  //Se comprueba si S03 Y S04 estan detectando algo--> el caso raro
             if(((PINK0&0x20)==0x20)&&((PINK&0x20)==0x00)&&((PINK&0x1C)==0x1C)){ //Se comprueba si el coche que ha ido para atras ha salido
-                Motor(M3,ON,IZQUIERDA);
+                motor(M3,ON,IZQUIERDA);
             }else{
-                Motor(M3,ON,DERECHA);   //El coche que ha ido para atras sigue bloqueando
+                motor(M3,ON,DERECHA);   //El coche que ha ido para atras sigue bloqueando
             }
         }    
         else{
             LH=1;
-            Motor(M4,ON,DERECHA);//EL SENTIDO HAY Q MIRARLO
+            motor(M4,ON,DERECHA);//EL SENTIDO HAY Q MIRARLO
         }
     }
     PINK0=PINK;
--- P1/lavado_horizontal.h
+++ P1/lavado_horizontal.h
@@ -3,6 +3,6 @@
 #ifndef LAVADO_HORIZONTAL_H_
 #define LAVADO_HORIZONTAL_H_
 
-void lavado_horizontal();
+
 
 #endif 
