09e03b98aaf1ad86ccf2b71740b4a5da8a71980e
chema <chemabc@gmail.com>
2022-05-11 17:36:04 +0200

    entrada.h working

--- P3/entrada.h
+++ P3/entrada.h
@@ -1,25 +1,11 @@
-
 #include "commonstuff.h"
 
 #ifndef ENTRADA_H_
 #define ENTRADA_H_
 
-char barrierPulseCounter = 0;
-bool barrierUp;
-bool barrierDown;
-
-bool carWaiting = false;
-seconds_t tPreviousCar = 0;
-
-miliseconds_t antireb_S01 = 0;
-miliseconds_t antireb_SW1 = 0;
-
 void barrera(barrier_status_t);
 void barrierMove(void);
-void barrierStope(void);
-void isbarrierDown(void);       // ??? necesita revisión
-// void tunnelGotBusy(void);    // ??? necesita revisión
-void barrierControl(status_t);  // debe terminarse
+void barrierStop(void);
+void barrierControl(mode_t);
 
 #endif /* ENTRADA_H_ */
-
