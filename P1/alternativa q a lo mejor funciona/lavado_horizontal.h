e242f2c530f98d5472e81ccbdd71dfe440c1145c
mariogomezpozo <94970439+mariogomezpozo@users.noreply.github.com>
2022-05-17 19:52:12 +0200

    LH y SH sin utilizar interrupciones

---
+++ P1/alternativa q a lo mejor funciona/lavado_horizontal.h
@@ -0,0 +1,10 @@
+#include "commonstuff.h"
+
+#ifndef LAVADO_HORIZONTAL_H_
+#define LAVADO_HORIZONTAL_H_
+
+void lavado_horizontal();
+void setup_LH_PORTS();
+void gestionLH(mode_t modo);
+
+#endif
---
+++ P1/alternativa q a lo mejor funciona/secado_horizontal.h
@@ -0,0 +1,10 @@
+#include "commonstuff.h"
+
+#ifndef SECADO_HORIZONTAL_H_
+#define SECADO_HORIZONTAL_H_
+
+void secado_horizontal();
+void setup_SH_PORTS();
+void gestionSH(mode_t modo);
+
+#endif
