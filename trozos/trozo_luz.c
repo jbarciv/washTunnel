60e3d23d0d63f637ef59ad163e488007bba4e6b0
gqt124 <gonzaloytorres@gmail.com>
2022-05-19 13:38:51 +0200

    trozo luz

---
+++ trozos/trozo_luz.c
@@ -0,0 +1,21 @@
+void parpadeo(seconds_t ms, bool coche)
+{
+	if (coche)
+	{
+		if(ms % 2)			        // en los medios segundos "pares" está encendida
+        {
+            luz(L1, ON);
+        }
+        else luz(L1, OFF);
+	}
+	else 
+	{
+        luz(L1, OFF);
+		s_sin_coche++;
+        if ((s_sin_coche % 20) == 0) // si han pasado 20 medios de segundo (10s), se enciende
+        {
+            luz(L1, ON);
+            s_sin_coche = 0;
+        }
+	}
+}
